dff46724806db5f41517e9b92aed78abdeb1c0b0
mdavis29 <mdavis29@gmail.com>
2017-02-18 14:53:21 -0500

    fix to null

--- R/getSymbolList.R
+++ R/getSymbolList.R
@@ -1,19 +1,24 @@
 #' @title Get Market Indicators
-#' @param symbol.list  a vector of or list of symbols to look up
+#' @param symbol.list a vector of or list of symbols to look up
 #' @param st Date class object start date of when to look up
+#' @param aa Logical auto assign the stock symbol to the enviroment
 #' @author Matthew Davis
 #' @export
 #' @description looks up a list of stock tickers and returns a merged xts of them
 #' @return xts object of merged stock OHCL from the list provided
-getSymbolList<-function(symbol.list =c('^DJI','GSPC','NDAQ', '^N225','^GDAXI', '^FTSE'),
-                         st = as.Date("2000-01-01"), aa = FALSE){
+getSymbolList<-function(symbol.list = NULL ,
+                        st = as.Date("1999-02-01"), 
+                        aa = FALSE){
+  
+  if(is.null(symbol.list)){symbol.list<-c('^DJI','GSPC','NDAQ', '^N225','^GDAXI', '^FTSE')}
+  
   output.merged<-xts()
     for (i in 1:(length(symbol.list))){
         temp<-xts()
-        temp<-try(getSymbols(symbol.list[i], startDate = st, auto.assign = aa, src = "yahoo"))
+        temp<-try(getSymbols(symbol.list[i], from = st, auto.assign = aa, src = "yahoo"))
 
           if(is.null(nrow(temp))){
-             temp<-try(getSymbols(symbol.list[i], startDate = st, auto.assign = aa, src = "google"))
+             temp<-try(getSymbols(symbol.list[i], from = st, auto.assign = aa, src = "google"))
                 if(is.null(nrow(temp))){print(paste(symbol.list[i], 'failed to fetch Data'))}
                                 }
               output.merged<-merge.xts(output.merged, temp)
---
+++ R/updateXts.R
@@ -0,0 +1,23 @@
+#' @title Update my merged List of stock tickers 
+#' @param symbol.list  a merged xts object where the columns are stock OHLC Data 
+#' @author Matthew Davis
+#' @export
+#' @description looks up a list of stock tickers and returns a merged xts of them
+#' @return merged xts of stock tickers updated to the previous day.
+updateXts<-function(mydata){
+  getList<-c()
+  tickerList <- unlist(unique(lapply(colnames(mydata), 
+                             function(x)strsplit(x,'[.]')[[1]][[1]])))
+  maxDate <- as.Date(tail(index(mydata),1))
+  minDate <- as.Date(head(index(mydata),1))
+  for ( i in 1:length(tickerList)){
+    testHead<-anyNA(head(mydata[,grepl(tickerList[i], colnames(mydata))],1))
+    testTail<-anyNA(tail(mydata[,grepl(tickerList[i], colnames(mydata))],1))
+    if(all(c(testHead, testTail, maxDate < Sys.Date()-1 )) == FALSE){
+      getList<-append(getList, tickerList[i])
+        }
+      }
+    newData<-getSymbolList(getList,st = minDate, aa = FALSE )
+    output.merged<-merge.xts(mydata, newData)
+    return(output.merged)
+  }
